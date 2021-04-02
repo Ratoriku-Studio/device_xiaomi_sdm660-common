7ffd31e25b62e1d2c8cbd551a1d78b30ace00221
Hridya Valsaraju <hridya@google.com>
2021-04-02 11:33:45 -0700

    sdm660-common: Set PRODUCT_SET_DEBUGFS_RESTRICTIONS
    Starting with Android R launched devices, debugfs cannot be mounted in
    production builds. In order to avoid accidental debugfs dependencies
    from creeping in during development with userdebug/eng builds, the
    build flag PRODUCT_SET_DEBUGFS_RESTRICTIONS can be set by vendors to
    enforce additional debugfs restrictions for userdebug/eng builds. The
    same flag will be used to enable sepolicy neveallow statements to
    prevent new permissions added for debugfs access.
    
    Test: build, boot
    Bug: 184381659
    Change-Id: I45e6f20c886d467a215c9466f3a09965ff897d7e

--- sdm660.mk
+++ sdm660.mk
@@ -158,6 +158,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 endif
 
+# DebugFS
+PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
+
 # Display
 PRODUCT_PACKAGES += \
     gralloc.sdm660 \
