95687279eb22fb515d72ea50e8dc8902e7148205
Alin Jerpelea <alin.jerpelea@sony.com>
2023-02-18 17:26:55 +0800

    sdm660-common: configs: Add qmi_fw.conf
    This file will silence the warnings on qmi
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>
    Change-Id: I7d7ec0da00a33a0da8f189b0181d40919b9918e5

--- sdm660.mk
+++ sdm660.mk
@@ -375,6 +375,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libjson
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qmi_fw.conf:$(TARGET_COPY_OUT_VENDOR)/etc/qmi_fw.conf
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.2.vendor \
