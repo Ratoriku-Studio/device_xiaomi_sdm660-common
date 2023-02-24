f23409e256a2f2cf30a48fa5814e2b71e884f9e7
johnmart19 <johnivan19999@gmail.com>
2023-02-24 14:19:55 +0800

    sdm660-common: device: Import android.hardware.media.c2 to FCM
    * Also build android.hardware.media.c2@1.2.vendor
    
    hwservicemanager: Since
    android.hardware.media.c2@1.0::IComponentStore/default is not
    registered, trying to start it as a lazy HAL.
    CodecServiceRegistrant: Preferred Codec2 store is defaulted to
    "software".
    CodecServiceRegistrant: Software Codec2 service created and registered.
    libc    : Unable to set property "ctl.interface_start" to
    "android.hardware.media.c2@1.0::IComponentStore/default": error code:
    0x20
    hwservicemanager: Tried to start
    android.hardware.media.c2@1.0::IComponentStore/default as a lazy
    service, but was unable to. Usually this happens when a service is not
    installed, but if the service is intended to be used as a lazy service,
    then it may be configured incorrectly.

--- sdm660.mk
+++ sdm660.mk
@@ -359,7 +359,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
-    android.hardware.media.c2@1.0.vendor \
+    android.hardware.media.c2@1.2.vendor \
     libcodec2_hidl@1.0.vendor \
     libcodec2_vndk.vendor
 
