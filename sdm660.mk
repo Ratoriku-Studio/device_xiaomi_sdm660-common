9116eadc0f91bebf6357b11d623d3dc5f5caada9
Volodymyr Zhdanov <wight554@gmail.com>
2023-04-17 23:16:52 +0530

    sdm660-common: Disable configstore usage
    * it's finally deprecated in R
    * ship disable_configstore because we have API level less than 29

--- sdm660.mk
+++ sdm660.mk
@@ -153,6 +153,10 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# Configstore (Disabled)
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
