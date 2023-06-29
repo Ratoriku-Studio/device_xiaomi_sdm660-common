875f7698473f16b33065dfc26defa6525a35cac3
D3nesyan <d3nesyan@jmu.edu.cn>
2023-06-29 10:25:38 +0800

    sdm660-common: Remove unused sched_enable_power_aware cmdline
    Signed-off-by: D3nesyan <d3nesyan@jmu.edu.cn>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -40,7 +40,6 @@ BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 \
                         msm_rtb.filter=0x37 \
                         ehci-hcd.park=3 \
                         lpm_levels.sleep_disabled=1 \
-                        sched_enable_power_aware=1 \
                         service_locator.enable=1 \
                         androidboot.configfs=true \
                         androidboot.usbcontroller=a800000.dwc3 \
